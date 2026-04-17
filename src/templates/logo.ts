27f51b616648523eb9804647226ee64109c7ba96
Joel <joeldario9912@gmail.com>
2026-04-17 16:22:07 -0300

    Wrap header title in link and adjust style

--- src/templates/logo.ts
+++ src/templates/logo.ts
@@ -1,5 +1,5 @@
 export const logoTemplate = () => {
   const logo = document.createElement("h1") as HTMLElement;
-  logo.innerText = `Food Store`;
+  logo.innerHTML = `<a href="/">Food Store</a>`;
   return logo;
 };
