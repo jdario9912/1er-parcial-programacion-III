8259666689f0980f41eab409531ece717f5e92b9
Joel <joeldario9912@gmail.com>
2026-04-20 14:56:05 -0300

    feat: counter visor added

--- src/templates/navbar.ts
+++ src/templates/navbar.ts
@@ -4,7 +4,7 @@ export const navBarTemplate = () => {
   navBar.innerHTML = `
     <ul>
       <li><a href="/src/pages/catalog/catalog.html">Catalog</a></li>
-      <li><a href="/src/pages/cart/cart.html">Cart</a></li>
+      <li><a href="/src/pages/cart/cart.html">Cart<span id="cart-counter"></span></a></li>
     </ul>
   `;
 
